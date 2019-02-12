eb4e2cc37f81b2e9ca0b8ef49533a1d1593197f4
Charlotte Lau <tweety.tinglau@gmail.com>
2019-02-12 11:47:18 +0800

    change destination path

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+    output: {
+        path:__dirname + '/build'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.html$/,
+            use: [
+            {
+                loader: "html-loader"
+            }
+            ]
         }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
         ]
-      }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
