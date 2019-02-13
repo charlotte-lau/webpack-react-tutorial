2d5c63f8620cf62e4d604c819ae20f16d161eea0
Charlotte Lau <tweety.tinglau@gmail.com>
2019-02-13 12:44:58 +0800

    Header fix

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Glyphicon } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
 
 class Header extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                    <div className="navbar-header"><a href="/" className="navbar-brand">Our Awesome Store</a>
-                        <button type="button" className="navbar-toggle collapsed"><span className="sr-only">Toggle navigation</span><span
-                            className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
-                    </div>
-                    <div className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li role="presentation"><a role="button" href="#">Home</a></li>
-                            <li role="presentation"><a role="button" href="#">Shop</a></li>
-                            <li role="presentation"><a role="button" href="#"><span
-                                className="glyphicon glyphicon-shopping-cart"></span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        );
+            <Nav
+                activeKey="/home"
+                onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+                >
+                <Nav.Item>
+                    <Nav.Link href="/home">Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                    </Nav.Link>
+                </Nav.Item>
+                </Nav>
+        )
     }
 };
 
